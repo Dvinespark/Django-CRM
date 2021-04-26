ace4c752c9281f43f86adc24592f25cc59b4c03a
shubhas <nimtsubash@gmail.com>
2021-04-26 14:50:53 +0545

    template structure added for leads app

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Agent
 # Create your views here.
+
+def index(request):
+    # return HttpResponse("Hello World")
+    agent = Agent.objects.all().first()
+    data = {
+        'data': agent
+    }
+    
+    return render(request, "leads/index.html", context=data)
